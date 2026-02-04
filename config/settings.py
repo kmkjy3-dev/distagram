467dfa575dd04dcdf8d259b5cd6160ba6227b050
kmkjy3-dev <kmkjy3@gmail.com>
2026-02-04 17:14:00 +0900

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-l-sqyvd7-ve2cg2!r^rwpi^dc)&*0kagv(ifksjn%i&w4(=8)x"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['JaehongOh.pythonanywhere.com']
 
